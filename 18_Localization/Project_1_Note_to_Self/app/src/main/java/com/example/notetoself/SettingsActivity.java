96a35834be1ff804bae37b186dc7d1688492c9e0
syndicate017 <geniuses17@gmail.com>
2021-11-30 03:50:22 +0700

    18 - Localization / Project 1 - Note to Self

---
+++ 18_Localization/Project_1_Note_to_Self/app/src/main/java/com/example/notetoself/SettingsActivity.java
@@ -0,0 +1,53 @@
+package com.example.notetoself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class SettingsActivity extends AppCompatActivity {
+    private SharedPreferences mPrefs;
+    private SharedPreferences.Editor mEditor;
+
+    private boolean mShowDividers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        mPrefs = getSharedPreferences("Note to Self", MODE_PRIVATE);
+        mEditor = mPrefs.edit();
+
+        mShowDividers = mPrefs.getBoolean("dividers", true);
+
+        Switch switch1 = findViewById(R.id.switch1);
+        //Set the switch on or off as appropriate
+        switch1.setChecked(mShowDividers);
+
+        switch1.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            mEditor.putBoolean("dividers", true);
+                        } else {
+                            mEditor.putBoolean("dividers", false);
+                        }
+                    }
+                }
+        );
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Save the settings here
+        mEditor.commit();
+    }
+}
