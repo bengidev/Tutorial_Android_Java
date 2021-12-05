6f9a3fadc3cfaac88f71827c77365d9980584fa5
syndicate017 <geniuses17@gmail.com>
2021-12-06 01:35:01 +0700

    24 - Design Patterns, Multiple Layouts, and Fragments / Project 2 - Simple Fragment

---
+++ 24_Design_Patterns_Multiple_Layouts_and_Fragments/Project_2_Simple_Fragment/app/src/main/java/com/example/simplefragment/SimpleFragment.java
@@ -0,0 +1,41 @@
+package com.example.simplefragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SimpleFragment extends Fragment {
+    //Member variables accessible from anywhere in this fragment
+    String myString;
+    Button myButton;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        myString = "Hello from SimpleFragment";
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_layout, container, false);
+
+        myButton = view.findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), myString, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
